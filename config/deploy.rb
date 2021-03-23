3707c475c2406bb8c34b52df2e3619ffcd49afa2
georgeapostolopoulos <georgea@cpdi-inc.com>
2021-03-23 15:21:58 -0500

    removed duplicates. Added configuration to Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "cpdi-master"
-set :repo_url, "git@github.com:pierreyfl/cpdi-2.git"
-set :deploy_to, "/var/www/cpdi-master"
+set :application, "myapp"
+set :repo_url, "https://github.com/george-cpdi/CPDIPROJECT.git"
+set :deploy_to, "/home/serveradmin/myapp"
 set :branch, 'master'
 set :keep_releases, 5
 set :format, :pretty
 set :log_level, :debug
 set :pty, true
-
